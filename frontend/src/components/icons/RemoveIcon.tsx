c63b43dc82c16365501978cf71830b9e1e5545e6
Nicolas Philipp <e12216446@student.tuwien.ac.at>
2024-08-25 13:00:23 +0200

    added edit and remove icons

---
+++ frontend/src/components/icons/RemoveIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function RemoveIcon(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="96px" height="96px" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12z"></path></svg>);
+}
+
+//https://icon-sets.iconify.design/mdi/remove-bold/
