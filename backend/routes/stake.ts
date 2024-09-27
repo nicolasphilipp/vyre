9b7748f7d95e8bd6125daa1842fa13404dabfc1c
Nicolas Philipp <e12216446@student.tuwien.ac.at>
2024-09-27 16:49:04 +0200

    added support for cexplorer api to get stake pool data

--- backend/routes/stake.ts
+++ backend/routes/stake.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response} from 'express';
 import { getPool } from '../utils/stake';
+import * as fs from 'fs';
 const routes = express.Router();
 
+const CEXPLORER_API = "https://js.cexplorer.io/api-static/pool/";
+
 routes.get('/:id', async (req: Request, res: Response) => {
-    if(req.query.stake) {
-        let stake = parseInt(req.query.stake as string);
-        
-        let pool = JSON.parse(await getPool(stake, req.params.id));
-        if(pool.error){
-            res.status(500).send(JSON.stringify({ error: pool.error }));
-        } else { 
-            res.status(200).send(JSON.stringify({ pool: pool.pool }));
-        }
-    }
+    // TEMPORARY
+    const data = fs.readFileSync("./resources/atada.json", 'utf-8');
+    const jsonData = JSON.parse(data);
+
+    res.status(200).send(JSON.stringify({ pool: jsonData.data }));
+
+    /*fetch(CEXPLORER_API + req.params.id + ".json")
+        .then(res => res.json())
+        .then(res => {
+            res.status(200).send(JSON.stringify({ pool: res.data }));
+        })
+        .catch(err => {
+            res.status(500).send(JSON.stringify({ error: err }));
+        });*/
 });
 
 export default routes;
