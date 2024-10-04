29fe007592f073c9ffdab75d823465b3c0992ce0
Nicolas Philipp <e12216446@student.tuwien.ac.at>
2024-10-04 23:55:19 +0200

    added new icons

---
+++ frontend/src/components/icons/ChartLineIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function ChartLineIcon(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={96} height={96} viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path d="M3 3v16a2 2 0 0 0 2 2h16"></path><path d="m19 9l-5 5l-4-4l-3 3"></path></g></svg>);
+}
+
+// https://icon-sets.iconify.design/lucide/chart-line/
